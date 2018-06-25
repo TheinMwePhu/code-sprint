08ae80ba9cd04261eaed86e02577f0b09fdf5ed8
TheinMwePhu <theinmwephu97@gmail.com>
2018-06-25 12:05:30 +0630

    printing,printing

---
+++ python-exercises/ex8.py
@@ -0,0 +1,11 @@
+formatter = " {} {} {} {} "
+print(formatter.format(1, 2, 3, 4))
+print(formatter.format("One", "two", "three", "four"))
+print(formatter.format(True, False, True, False))
+print(formatter.format(formatter, formatter, formatter, formatter))
+print(formatter.format(
+    "Try you",
+    "Own text here",
+    "May be  a poen",
+    "Or a song about fear"
+    ))
